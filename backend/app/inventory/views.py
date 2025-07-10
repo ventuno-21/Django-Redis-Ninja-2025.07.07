1981d600fe0eab862ae1b8f18d23b6de2746c840
ventuno-21 <yasna.pakdel@outlook.com>
2025-07-09 23:38:44 -0700

    Defining ProductSerlizers to access all products and related functionalty adn url

--- backend/app/inventory/views.py
+++ backend/app/inventory/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductListAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
