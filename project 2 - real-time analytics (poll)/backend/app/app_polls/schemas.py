ff235c5be37e43b8ed7a52240253d408c6825f0a
ventuno-21 <yasna.pakdel@outlook.com>
2025-07-19 00:41:50 -0700

    simple post request with django NINJA

--- project 2 - real-time analytics (poll)/backend/app/app_polls/schemas.py
+++ project 2 - real-time analytics (poll)/backend/app/app_polls/schemas.py
@@ -1,6 +1,20 @@
 from ninja import Schema
+from typing import Dict
 
 
 class PollOut(Schema):
     id: int
     question: str
+
+
+class CreatePoll(Schema):
+    question: str
+    text: Dict[str, str]
+
+
+class CreatePollOut(CreatePoll):
+    id: int
+
+
+class ErrorSchema(Schema):
+    error: str
--- project 2 - real-time analytics (poll)/backend/app/app_polls/views.py
+++ project 2 - real-time analytics (poll)/backend/app/app_polls/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from ninja import Router
 
 from .models import Poll
-from .schemas import PollOut
+from .schemas import CreatePoll, CreatePollOut, PollOut, ErrorSchema
 
 # Create your views here.
 
@@ -16,3 +16,14 @@ router = Router()
 async def poll_list(request):
     polls = await sync_to_async(list)(Poll.objects.all())
     return polls
+
+
+@router.post("/polls/add", response={201: CreatePollOut, 400: ErrorSchema})
+async def create_poll(request, data: CreatePoll):
+
+    if not data.text or len(data.text) < 2:
+        return 400, {"error": "At least two poll options are required"}
+    poll = Poll(question=data.question, text=data.text)
+    await poll.asave()
+    # return 201, CreatePollOut(id=poll.id, question=poll.question, text=poll.text)
+    return 201, poll
